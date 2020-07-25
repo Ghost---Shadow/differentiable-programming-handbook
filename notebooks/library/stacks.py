5514d40e596aef4c2b19c9ef9364f3d3cf3e4f7b
Souradeep Nanda <Ghost---Shadow@users.noreply.github.com>
2020-07-25 19:38:02 +0530

    Vectorized stacks

--- notebooks/library/stacks.py
+++ notebooks/library/stacks.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 
-from library.array_ops import assign_index
+from library.array_ops import assign_index_vectored, superposition_lookup_vectored
 
 
 @tf.function
 def stack_push(state, element):
     buffer, index = state
-    buffer = assign_index(buffer, index, element)
-    index += 1
-    
+    buffer = assign_index_vectored(buffer, index, element)
+    index = tf.roll(index, shift=1, axis=0)
     state = (buffer, index)
     return state
 
@@ -16,8 +15,7 @@ def stack_push(state, element):
 @tf.function
 def stack_pop(state):
     buffer, index = state
-    index -= 1
-    element = buffer[index]
-    
+    index = tf.roll(index, shift=-1, axis=0)
+    element = superposition_lookup_vectored(buffer, index)
     state = (buffer, index)
     return state, element
